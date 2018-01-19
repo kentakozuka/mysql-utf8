d0f8ad3d20a798814af2ea960e61de7ab24d3211
kenta <kenta@localhost.localdomain>
2018-01-20 00:22:59 +0900

    add a line for SELinux

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
+#/bin/sh
+
+# for SELinux
+su -c "setenforce 0"
+
 docker run \
 		-it \
 		-d \
 		--name db \
-		-v db_data:/var/lib/mysql \
+		-v ${PWD}/db_data:/var/lib/mysql \
 		-e MYSQL_ROOT_PASSWORD=root \
 		-p 3306:3306 \
 		mysql:5.7 \
